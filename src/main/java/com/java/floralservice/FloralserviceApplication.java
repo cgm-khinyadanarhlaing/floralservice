29d714c01f0d9c50d698baa8e4fccbd7279b92d5
Khin Yadanar Hlaing <khinyadanarhlaing@cgm-myanmar.com>
2024-03-04 11:48:46 +0630

    git first commit

---
+++ src/main/java/com/java/floralservice/FloralserviceApplication.java
@@ -0,0 +1,13 @@
+package com.java.floralservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloralserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloralserviceApplication.class, args);
+	}
+
+}
